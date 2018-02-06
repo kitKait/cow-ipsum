3529a96950b07d3bdf441703d075a3fdeff9d367
ksykes <kait@kaitsykes.com>
2018-02-06 12:18:15 -0500

    proper sentence structure + paragraph generator

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -2,9 +2,12 @@
 
 "use strict";
 
-var cowIpsumApp = {};
+let cowIpsumApp = {};
+cowIpsumApp.cowSentence = {};
+cowIpsumApp.properSentence = {};
+cowIpsumApp.paragraph = {};
 
-var cowWordList = ["Angus", "Holstein Friesian", "Shorthorn", "Hereford", "Galloway", "Brown Swiss", "Texas Longhorn", "Ayrshire", "Brangus", "Jersey", "Highland", "Beefmaster", "Dexter", "Guernsey", "Belgian Blue", "Red Poll", "English Longhorn", "Piedmontese", "Aubrac", "Bonsmara", "Normande", "Brahman", "Charolais", "Gelbvieh", "Limousin", "Red Angus", "Simmental", "Watusi", "cattle", "cow", "calf", "heifer", "steer", "bullocks", "ox", "springer", "freemartin", "moooooo", "milk", "beef", "udder", "dairy", "bovine", "bull", "cowshed", "barn", "cow patty", "lowing", "grass", "hay", "chew your cud", "holy cow", "herd", "livestock", "cream", "whole milk", "half and half", "chocolate milk", "churn", "butter", "whey", "domesticated ungulates", "bos taurus", "farm", "horns", "cattle raiding", "bison", "zebu", "yak", "milker", "house cow", "oxtail", "pasture"];
+const cowWordList = ["Angus", "Holstein Friesian", "Shorthorn", "Hereford", "Galloway", "Brown Swiss", "Texas Longhorn", "Ayrshire", "Brangus", "Jersey", "Highland", "Beefmaster", "Dexter", "Guernsey", "Belgian Blue", "Red Poll", "English Longhorn", "Piedmontese", "Aubrac", "Bonsmara", "Normande", "Brahman", "Charolais", "Gelbvieh", "Limousin", "Red Angus", "Simmental", "Watusi", "cattle", "cow", "calf", "heifer", "steer", "bullocks", "ox", "springer", "freemartin", "moooooo", "milk", "beef", "udder", "dairy", "bovine", "bull", "cowshed", "barn", "cow patty", "lowing", "grass", "hay", "chew your cud", "holy cow", "herd", "livestock", "cream", "whole milk", "half and half", "chocolate milk", "churn", "butter", "whey", "domesticated ungulates", "bos taurus", "farm", "horns", "cattle raiding", "bison", "zebu", "yak", "milker", "house cow", "oxtail", "pasture"];
 
 console.log('hello world');
 console.log(cowWordList);
@@ -17,19 +20,43 @@ let newSentencesArray = [];
 
 // generate 10 random words from the cowWordList, and make a sentence with the words
 const generateCowWords = () => {
-	for (let i = 0; i < 10; i++) {
+	newWordsArray = [];
+	for (let i = 0; i < 11; i++) {
 		let randoNum = Math.floor(Math.random() * cowWordList.length);
 		let randoWord = cowWordList[randoNum];
-		console.log(randoWord);
+		// console.log(randoWord);
 		newWordsArray.push(randoWord);
 	}
-	console.log(newWordsArray);
-	let cowSentence = newWordsArray.join(' ');
-	console.log(cowSentence);
+	// console.log(newWordsArray);
+	// add the random words to an array with a space in between
+	cowIpsumApp.cowSentence = newWordsArray.join(' ');
+	console.log(cowIpsumApp.cowSentence);
+	// capitalize and period-ize the sentence
+	cowIpsumApp.properSentence = cowIpsumApp.cowSentence.charAt(0).toUpperCase() + cowIpsumApp.cowSentence.slice(1) + '.';
+	console.log(cowIpsumApp.properSentence);
+	// console.log(typeof cowIpsumApp.properSentence);
+	// make array of 10 sentences
+	newSentencesArray.push(cowIpsumApp.properSentence);
+	console.log(newSentencesArray);
+	// console.log(typeof newSentencesArray);
 }
 
-generateCowWords();
+// make a paragraph of 8 sentences
+const generateParagraph = () => {
+	// run sentence generator
+	for (let i = 0; i < 9; i++) {
+		generateCowWords();
+	}
+	// convert the array of sentences to one string with a space after
+	cowIpsumApp.paragraph = newSentencesArray.join(' ');
+	console.log(cowIpsumApp.paragraph);
+}
 
-$(document).ready(function () {
-	//
+cowIpsumApp.init = () => {
+	generateCowWords();
+	generateParagraph();
+}
+
+$(function() {
+	cowIpsumApp.init();
 });
