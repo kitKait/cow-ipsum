a7baa0792d909a239ab3a0a2e1571d7b83e0410d
Kitty <kittyyau8@gmail.com>
2018-02-08 21:57:08 -0500

    fix naming conventions and add event listeners

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,65 +1,73 @@
-// 10 words per sentence; 8 sentences per paragraph (80 words)
-
 "use strict";
 
 let cowIpsumApp = {};
-cowIpsumApp.cowSentence = {};
-cowIpsumApp.properSentence = {};
-cowIpsumApp.paragraph = {};
+cowIpsumApp.sentence;
+cowIpsumApp.paragraph;
+cowIpsumApp.newWordsArray = [];
+cowIpsumApp.newSentencesArray = [];
 
-const cowWordList = ["Angus", "Holstein Friesian", "Shorthorn", "Hereford", "Galloway", "Brown Swiss", "Texas Longhorn", "Ayrshire", "Brangus", "Jersey", "Highland", "Beefmaster", "Dexter", "Guernsey", "Belgian Blue", "Red Poll", "English Longhorn", "Piedmontese", "Aubrac", "Bonsmara", "Normande", "Brahman", "Charolais", "Gelbvieh", "Limousin", "Red Angus", "Simmental", "Watusi", "cattle", "cow", "calf", "heifer", "steer", "bullocks", "ox", "springer", "freemartin", "milk", "beef", "udder", "dairy", "bovine", "bull", "cowshed", "barn", "cow patty", "lowing", "grass", "hay", "chew your cud", "holy cow", "herd", "livestock", "cream", "whole milk", "half and half", "chocolate milk", "churn", "butter", "whey", "domesticated ungulates", "bos taurus", "farm", "horns", "cattle raiding", "bison", "zebu", "yak", "milker", "house cow", "oxtail", "pasture"];
+cowIpsumApp.cowWordList = ["Angus", "Holstein Friesian", "Shorthorn", "Hereford", "Galloway", "Brown Swiss", "Texas Longhorn", "Ayrshire", "Brangus", "Jersey", "Highland", "Beefmaster", "Dexter", "Guernsey", "Belgian Blue", "Red Poll", "English Longhorn", "Piedmontese", "Aubrac", "Bonsmara", "Normande", "Brahman", "Charolais", "Gelbvieh", "Limousin", "Red Angus", "Simmental", "Watusi", "cattle", "cow", "calf", "heifer", "steer", "bullocks", "ox", "springer", "freemartin", "milk", "beef", "udder", "dairy", "bovine", "bull", "cowshed", "barn", "cow patty", "lowing", "grass", "hay", "chew your cud", "holy cow", "herd", "livestock", "cream", "whole milk", "half and half", "chocolate milk", "churn", "butter", "whey", "domesticated ungulates", "bos taurus", "farm", "horns", "cattle raiding", "bison", "zebu", "yak", "milker", "house cow", "oxtail", "pasture"];
 
-console.log('hello world');
-console.log(cowWordList);
 
-// empty array to store selected words
-let newWordsArray = [];
-
-// variable to store one sentence 
-let cowSentence;
-
-// empty array to store selected sentences
-let newSentencesArray = [];
-
-// variable to store paragraph
-let cowParagraph;
-
-// generate 10 random words from the cowWordList, and make a sentence with the words
-const generateCowWords = () => {
-	newWordsArray = [];
+// generate 10 random words from the cowWordList
+cowIpsumApp.generateCowWords = () => {
+	cowIpsumApp.newWordsArray = [];
 	for (let i = 0; i < 11; i++) {
-		let randoNum = Math.floor(Math.random() * cowWordList.length);
-		let randoWord = cowWordList[randoNum];
-		// console.log(randoWord);
-		newWordsArray.push(randoWord);
+		let randoNum = Math.floor(Math.random() * cowIpsumApp.cowWordList.length);
+		let randoWord = cowIpsumApp.cowWordList[randoNum];
+		cowIpsumApp.newWordsArray.push(randoWord);
 	}
 }
 
-const generateSentence = () => {
+// Make a sentence with the 10 random words generated from the cowIpsumApp.generateCowWords function
+cowIpsumApp.generateSentence = () => {
+	cowIpsumApp.generateCowWords();
 	// add the random words to an array with a space in between
-	cowIpsumApp.makeSentence = newWordsArray.join(' ');
-	// console.log(cowIpsumApp.cowSentence);
+	let makeSentence = cowIpsumApp.newWordsArray.join(' ');
 	// capitalize and period-ize the sentence
-	cowSentence = cowIpsumApp.makeSentence.charAt(0).toUpperCase() + cowIpsumApp.makeSentence.slice(1) + '.';
-	console.log(cowSentence);
+	cowIpsumApp.sentence = makeSentence.charAt(0).toUpperCase() + makeSentence.slice(1) + '.';
+	$('.displayIpsum').html(`<p>${cowIpsumApp.sentence}</p>`);
+	return cowIpsumApp.sentence;
 }
 
-// make a paragraph of 8 sentences
-const generateParagraph = () => {
+// Make a paragraph of 6 sentences
+cowIpsumApp.generateParagraph = () => {
+	cowIpsumApp.newSentencesArray = [];
 	// run sentence generator
-	for (let i = 0; i < 9; i++) {
-		generateSentence();
-		newSentencesArray.push(cowSentence);
+	for (let i = 0; i < 7; i++) {
+		let newSentence = cowIpsumApp.generateSentence();
+		cowIpsumApp.newSentencesArray.push(newSentence);
 	}
 	// convert the array of sentences to one string with a space after
-	cowParagraph = newSentencesArray.join(' ').slice(0, -1) + ', moooooooo.';
-	console.log(cowParagraph);
+	cowIpsumApp.paragraph = cowIpsumApp.newSentencesArray.join(' ').slice(0, -1) + ', moooooooo.';
+	$('.displayIpsum').html(`<p>${cowIpsumApp.paragraph}</p>`);
 }
 
+// Method to copy cow text
+cowIpsumApp.copyText = () => {
+	console.log('meep');
+}
+
+// event listeners
+cowIpsumApp.events = () => {
+	$('#sentenceButton').on('click', (e) => {
+		e.preventDefault();
+		cowIpsumApp.generateSentence();
+	});
+	
+	$('#paragraphButton').on('click', (e) => {
+		e.preventDefault();
+		cowIpsumApp.generateParagraph();
+	});
+
+	$('#copyButton').on('click', (e) => {
+		e.preventDefault();
+	});
+}
+
+
 cowIpsumApp.init = () => {
-	generateCowWords();
-	generateSentence();
-	generateParagraph();
+	cowIpsumApp.events();
 }
 
 $(function() {
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,65 +1,71 @@
-// 10 words per sentence; 8 sentences per paragraph (80 words)
-
 "use strict";
 
 var cowIpsumApp = {};
-cowIpsumApp.cowSentence = {};
-cowIpsumApp.properSentence = {};
-cowIpsumApp.paragraph = {};
+cowIpsumApp.sentence;
+cowIpsumApp.paragraph;
+cowIpsumApp.newWordsArray = [];
+cowIpsumApp.newSentencesArray = [];
 
-var cowWordList = ["Angus", "Holstein Friesian", "Shorthorn", "Hereford", "Galloway", "Brown Swiss", "Texas Longhorn", "Ayrshire", "Brangus", "Jersey", "Highland", "Beefmaster", "Dexter", "Guernsey", "Belgian Blue", "Red Poll", "English Longhorn", "Piedmontese", "Aubrac", "Bonsmara", "Normande", "Brahman", "Charolais", "Gelbvieh", "Limousin", "Red Angus", "Simmental", "Watusi", "cattle", "cow", "calf", "heifer", "steer", "bullocks", "ox", "springer", "freemartin", "milk", "beef", "udder", "dairy", "bovine", "bull", "cowshed", "barn", "cow patty", "lowing", "grass", "hay", "chew your cud", "holy cow", "herd", "livestock", "cream", "whole milk", "half and half", "chocolate milk", "churn", "butter", "whey", "domesticated ungulates", "bos taurus", "farm", "horns", "cattle raiding", "bison", "zebu", "yak", "milker", "house cow", "oxtail", "pasture"];
+cowIpsumApp.cowWordList = ["Angus", "Holstein Friesian", "Shorthorn", "Hereford", "Galloway", "Brown Swiss", "Texas Longhorn", "Ayrshire", "Brangus", "Jersey", "Highland", "Beefmaster", "Dexter", "Guernsey", "Belgian Blue", "Red Poll", "English Longhorn", "Piedmontese", "Aubrac", "Bonsmara", "Normande", "Brahman", "Charolais", "Gelbvieh", "Limousin", "Red Angus", "Simmental", "Watusi", "cattle", "cow", "calf", "heifer", "steer", "bullocks", "ox", "springer", "freemartin", "milk", "beef", "udder", "dairy", "bovine", "bull", "cowshed", "barn", "cow patty", "lowing", "grass", "hay", "chew your cud", "holy cow", "herd", "livestock", "cream", "whole milk", "half and half", "chocolate milk", "churn", "butter", "whey", "domesticated ungulates", "bos taurus", "farm", "horns", "cattle raiding", "bison", "zebu", "yak", "milker", "house cow", "oxtail", "pasture"];
 
-console.log('hello world');
-console.log(cowWordList);
-
-// empty array to store selected words
-var newWordsArray = [];
-
-// variable to store one sentence 
-var cowSentence = void 0;
-
-// empty array to store selected sentences
-var newSentencesArray = [];
-
-// variable to store paragraph
-var cowParagraph = void 0;
-
-// generate 10 random words from the cowWordList, and make a sentence with the words
-var generateCowWords = function generateCowWords() {
-	newWordsArray = [];
+// generate 10 random words from the cowWordList
+cowIpsumApp.generateCowWords = function () {
+	cowIpsumApp.newWordsArray = [];
 	for (var i = 0; i < 11; i++) {
-		var randoNum = Math.floor(Math.random() * cowWordList.length);
-		var randoWord = cowWordList[randoNum];
-		// console.log(randoWord);
-		newWordsArray.push(randoWord);
+		var randoNum = Math.floor(Math.random() * cowIpsumApp.cowWordList.length);
+		var randoWord = cowIpsumApp.cowWordList[randoNum];
+		cowIpsumApp.newWordsArray.push(randoWord);
 	}
 };
 
-var generateSentence = function generateSentence() {
+// Make a sentence with the 10 random words generated from the cowIpsumApp.generateCowWords function
+cowIpsumApp.generateSentence = function () {
+	cowIpsumApp.generateCowWords();
 	// add the random words to an array with a space in between
-	cowIpsumApp.makeSentence = newWordsArray.join(' ');
-	// console.log(cowIpsumApp.cowSentence);
+	var makeSentence = cowIpsumApp.newWordsArray.join(' ');
 	// capitalize and period-ize the sentence
-	cowSentence = cowIpsumApp.makeSentence.charAt(0).toUpperCase() + cowIpsumApp.makeSentence.slice(1) + '.';
-	console.log(cowSentence);
+	cowIpsumApp.sentence = makeSentence.charAt(0).toUpperCase() + makeSentence.slice(1) + '.';
+	$('.displayIpsum').html("<p>" + cowIpsumApp.sentence + "</p>");
+	return cowIpsumApp.sentence;
 };
 
-// make a paragraph of 8 sentences
-var generateParagraph = function generateParagraph() {
+// Make a paragraph of 6 sentences
+cowIpsumApp.generateParagraph = function () {
+	cowIpsumApp.newSentencesArray = [];
 	// run sentence generator
-	for (var i = 0; i < 9; i++) {
-		generateSentence();
-		newSentencesArray.push(cowSentence);
+	for (var i = 0; i < 7; i++) {
+		var newSentence = cowIpsumApp.generateSentence();
+		cowIpsumApp.newSentencesArray.push(newSentence);
 	}
 	// convert the array of sentences to one string with a space after
-	cowParagraph = newSentencesArray.join(' ').slice(0, -1) + ', moooooooo.';
-	console.log(cowParagraph);
+	cowIpsumApp.paragraph = cowIpsumApp.newSentencesArray.join(' ').slice(0, -1) + ', moooooooo.';
+	$('.displayIpsum').html("<p>" + cowIpsumApp.paragraph + "</p>");
+};
+
+// Method to copy cow text
+cowIpsumApp.copyText = function () {
+	console.log('meep');
+};
+
+// event listeners
+cowIpsumApp.events = function () {
+	$('#sentenceButton').on('click', function (e) {
+		e.preventDefault();
+		cowIpsumApp.generateSentence();
+	});
+
+	$('#paragraphButton').on('click', function (e) {
+		e.preventDefault();
+		cowIpsumApp.generateParagraph();
+	});
+
+	$('#copyButton').on('click', function (e) {
+		e.preventDefault();
+	});
 };
 
 cowIpsumApp.init = function () {
-	generateCowWords();
-	generateSentence();
-	generateParagraph();
+	cowIpsumApp.events();
 };
 
 $(function () {
